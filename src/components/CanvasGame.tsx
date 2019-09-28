b0b44e54022d1a829537a3c3654da12185003100
Alex Finkel <alex.finkel@stileeducation.com>
2019-09-29 00:27:58 +1000

    Git add the game wrapper - woops!

---
+++ src/components/CanvasGame.tsx
@@ -0,0 +1,45 @@
+import * as React from "react";
+import styled from "styled-components";
+
+import { Game } from "../classes/Game";
+
+export interface CanvasGameProps {}
+
+const Canvas = styled.canvas`
+    // width: 480px;
+    // height: 320px;
+    background: #eee;
+    display: block;
+`;
+export class CanvasGame extends React.Component<CanvasGameProps> {
+    game?: Game;
+    render() {
+        return (
+            <div>
+                <Canvas
+                    width={480}
+                    height={320}
+                    ref={canvasEl => {
+                        if (canvasEl) {
+                            this.game = new Game(canvasEl.getContext("2d"));
+                            this.game.start();
+                        } else {
+                            if (this.game) {
+                                this.game.pause();
+                            }
+                        }
+                    }}
+                />
+                <button onClick={() => this.game && this.game.pause()}>
+                    pause
+                </button>
+                <button onClick={() => this.game && this.game.start()}>
+                    play
+                </button>
+                <button onClick={() => this.game && this.game.reset()}>
+                    restart
+                </button>
+            </div>
+        );
+    }
+}
